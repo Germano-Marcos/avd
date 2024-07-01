67b9ddedc1c86a436ce81b848be1151604010b2d
Germano <germano.mja@gmail.com>
2024-07-01 06:46:27 -0300

    feat: create api register users

--- app.js
+++ app.js
@@ -2,14 +2,30 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
 
 app.use(express.json());
+
+
 
 // DB Connection
 const conn = require("./db/conn")
 
 conn()
+
+app.use((req, res, next) => {
+
+  res.header("Access-Control-Allow-Origin", "*")
+
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+
+  res.header("Access-Control-Allow-Headers", "Content-Type")
+
+  app.use(cors())
+
+  next()
+
+
+})
 
 // Routes
 const routes = require("./routes/router")
